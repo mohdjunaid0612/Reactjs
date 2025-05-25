632ed9449f95a707b14c62ba651c997cbf184a92
mohdjunaid0612 <mohdjunaid0612@gmail.com>
2025-05-25 22:22:23 +0530

    create new file effectUse

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import Product from './component/Product'
-import './App.css'
-import Events from './component/Events'
-import Counter from './component/Counter'
-import Map from './component/Map'
+import React from "react";
+import Product from "./component/Product";
+import "./App.css";
+import Events from "./component/Events";
+import Counter from "./component/Counter";
+import Map from "./component/Map";
+import EffectUse from "./component/EffectUse";
 
 const App = () => {
   return (
     <div>
       <h2>Props</h2>
       <div className="flex">
-        <Product title="Nord 3" brand="OnePlus" price="32,000"/>
-        <Product title="Nord CE 3" brand="OnePlus" price="22,000"/>
-        <Product title="Rezer CE 4" brand="OnePlus" price="54,000"/>
+        <Product title="Nord 3" brand="OnePlus" price="32,000" />
+        <Product title="Nord CE 3" brand="OnePlus" price="22,000" />
+        <Product title="Rezer CE 4" brand="OnePlus" price="54,000" />
       </div>
       <h2>Events</h2>
-      <Events/>
+      <Events />
       <h2>useState</h2>
-      <Counter/>
+      <Counter />
       <h2>Map</h2>
-      <Map/>
+      <Map />
+      <h2>useEffect</h2>
+      <EffectUse />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
